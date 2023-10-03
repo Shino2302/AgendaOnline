adf7ac6bd249bd1e93d63a1bb2de54cb360f914e
Shino2302 <encinas.sigfrido.cbtis37@gmail.com>
2023-10-03 14:15:54 -0700

    avance

---
+++ Models/UsuairosModel.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AgendaOnline.Models
+{
+    public class UsuairosModel
+    {
+        public int IdUsuario { get; set; }
+        public required string Nombre { get; set; }
+        public required string Apellido { get; set; }
+        public required string UserName { get; set; }
+        public required string Email { get; set; }
+        public required string Pass { get; set; }
+
+    }
+}
